ed340103675029bfd47c7083ff588d5559107cba
CDiaz-web <cdiazr4@gmai.com>
2026-03-18 17:17:21 -0500

    Actualiza ruta de alerta en inventario

--- includes/database.php
+++ includes/database.php
@@ -1,14 +1,34 @@
 <?php
+// $db = mysqli_connect(
+//     $_ENV['DB_HOST'] ?? '',
+//     $_ENV['DB_USER'] ?? '', 
+//     $_ENV['DB_PASS'] ?? '', 
+//     $_ENV['DB_NAME'] ?? ''
+// );
+
+// if (!$db) {
+//     echo "Error: No se pudo conectar a MySQL.";
+//     echo "errno de depuración: " . mysqli_connect_errno();
+//     echo "error de depuración: " . mysqli_connect_error();
+//     exit;
+// }
+
+
+
+function env_var($key, $default = '') {
+    return $_ENV[$key] ?? getenv($key) ?? $default;
+}
+
 $db = mysqli_connect(
-    $_ENV['DB_HOST'] ?? '',
-    $_ENV['DB_USER'] ?? '', 
-    $_ENV['DB_PASS'] ?? '', 
-    $_ENV['DB_NAME'] ?? ''
+    env_var('DB_HOST'),
+    env_var('DB_USER'),
+    env_var('DB_PASS'),
+    env_var('DB_NAME')
 );
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
+    echo " errno: " . mysqli_connect_errno();
+    echo " error: " . mysqli_connect_error();
     exit;
 }
--- views/admin/gestion/logistica/inventario/index.php
+++ views/admin/gestion/logistica/inventario/index.php
@@ -1,7 +1,8 @@
 
 <h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
    <?php 
-        include_once __DIR__ . '/../../../templates/alertas.php';          
+        include_once __DIR__ . '/../../../../templates/alertas.php';        
+          
     ?>
 
 
